550b0e4397163ef09170aedefe0db46f4dec549d
anil <abk33916@gmail.com>
2023-06-09 21:15:45 +0545

    red background was added

--- digitalclock.py
+++ digitalclock.py
@@ -4,9 +4,9 @@ from datetime import datetime
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
-window.configure(bg="steelblue")
+window.configure(bg="red")
 
-label = Label(window, font=("Arial Black", 78, "bold"), bg="steelblue", fg="white")
+label = Label(window, font=("Arial Black", 78, "bold"), bg="red", fg="white")
 label.pack(pady=100)
 
 def clock():
